8971ce573670a95b2d75a2626f7c1f1ec9f548fb
farxodnazarov <farhodnazarov002gmail.com>
2024-06-17 18:18:39 +0500

    site is ready

---
+++ src/footer/Footer.jsx
@@ -0,0 +1,19 @@
+//style
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container container">
+        <p>
+          All right Reserved :
+          <b>
+            <a href="https://t.me/nazarov_002"> My telegram link !</a>
+          </b>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
